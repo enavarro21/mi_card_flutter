72fc7500ba299b14dccb270bada7d3ca9c09d826
Erwin Navarro <erwin.r.navarro1@gmail.com>
2019-07-26 16:58:25 -0700

    Completed business card app for flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue[600],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundColor: Colors.blueGrey[300],
-                backgroundImage: AssetImage('images\erwin.jpg'),
+                backgroundColor: Colors.blue[600],
+                backgroundImage: AssetImage('images/erwin.jpg'),
+              ),
+              Text(
+                'Erwin Navarro',
+                style: TextStyle(
+                  fontFamily: 'LobsterTwo',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Exotic Snail Collector'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  color: Colors.blue[100],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.blue[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blue[500],
+                  ),
+                  title: Text(
+                    '(619) 245 1366',
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blue[500],
+                  ),
+                  title: Text(
+                    'erwin.r.navarro1@gmail.com',
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
